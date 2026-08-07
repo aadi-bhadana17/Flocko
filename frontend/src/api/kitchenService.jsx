4035f562ebbf742924a7285edc425e319c25960e
aadi-bhadana17 <aadibhadana114@gmail.com>
2026-08-07 23:37:57 +0530

    Update EntityGraph in FoodRepository to include only images for restaurant food retrieval

--- frontend/src/api/kitchenService.jsx
+++
@@ -1,7 +0,0 @@
-import api from './axiosConfig';
-
-export const updateKitchenStatus = async (restaurantId, kitchenLoadIndicator) => {
-    const response = await api.put(`/api/restaurant/${restaurantId}/kitchen/status`, { kitchenLoadIndicator });
-    return response.data;
-};
-
